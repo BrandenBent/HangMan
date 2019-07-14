80755781a161239237372e403b46e0175ab3e8af
BrandenBent <bent_branden@hotmail.com>
2019-07-13 22:57:03 -0600

    initial methods and ascii art

---
+++ HangMan.java
@@ -0,0 +1,133 @@
+package hangman;
+
+import java.util.Scanner;
+
+public class HangMan {
+
+	public static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+//		startGame();
+//		guessLetter();
+		System.out.println("test");
+		genWord();
+
+//		printHead();
+//		printBody();
+//		printLeftArm();
+//		printRightArm();
+//		printLeftLeg();
+//		printYouLose();
+
+	} // end main
+
+	public static void startGame() {
+		String startGame;
+		System.out.println("Welcome to Hangman, type \"Start\" to begin");
+		startGame = sc.next();
+		if (startGame.equalsIgnoreCase("start")) {
+			printHanger();
+
+		}
+	}
+
+	public static void genWord() {
+		System.out.println("test");
+
+	}
+
+	public static void guessLetter() {
+		String letterGuess;
+		System.out.println("Guess a letter A-Z");
+		letterGuess = sc.next();
+
+		// TODO add if statement checking if word contains letterGuess
+		System.out.println(letterGuess);
+
+	}
+
+	public static void printHanger() {
+		System.out.println("       _______ ");
+		System.out.println("      |       |");
+		System.out.println("              |");
+		System.out.println("              |");
+		System.out.println("              |");
+		System.out.println("              |");
+		System.out.println("              |");
+		System.out.println("       _______|_______ ");
+		System.out.println("      /_______|_______\\ \n");
+	}
+
+	public static void printHead() {
+		System.out.println("       _______ ");
+		System.out.println("      |       |");
+		System.out.println("      o       |");
+		System.out.println("              |");
+		System.out.println("              |");
+		System.out.println("              |");
+		System.out.println("              |");
+		System.out.println("       _______|________ ");
+		System.out.println("      /_______|________\\ \n");
+	}
+
+	public static void printBody() {
+		System.out.println("       _______ ");
+		System.out.println("      |       |");
+		System.out.println("      o       |");
+		System.out.println("      |       |");
+		System.out.println("      |       |");
+		System.out.println("              |");
+		System.out.println("              |");
+		System.out.println("       _______|________ ");
+		System.out.println("      /_______|________\\ \\n");
+	}
+
+	public static void printLeftArm() {
+		System.out.println("       _______ ");
+		System.out.println("      |       |");
+		System.out.println("      o       |");
+		System.out.println("    --|       |");
+		System.out.println("      |       |");
+		System.out.println("              |");
+		System.out.println("              |");
+		System.out.println("       _______|________ ");
+		System.out.println("      /_______|________\\ \\n");
+	}
+
+	public static void printRightArm() {
+		System.out.println("       _______ ");
+		System.out.println("      |       |");
+		System.out.println("      o       |");
+		System.out.println("    --|--     |");
+		System.out.println("      |       |");
+		System.out.println("              |");
+		System.out.println("              |");
+		System.out.println("       _______|________ ");
+		System.out.println("      /_______|________\\ \\n");
+	}
+
+	public static void printLeftLeg() {
+		System.out.println("       _______ ");
+		System.out.println("      |       |");
+		System.out.println("      o       |");
+		System.out.println("    --|--     |");
+		System.out.println("      |       |");
+		System.out.println("     /        |");
+		System.out.println("              |");
+		System.out.println("       _______|________ ");
+		System.out.println("      /_______|________\\ \\n");
+	}
+
+	public static void printYouLose() {
+		System.out.println("       _______ ");
+		System.out.println("      |       |");
+		System.out.println("      o       |");
+		System.out.println("    --|--     |");
+		System.out.println("      |       |");
+		System.out.println("     / \\      |");
+		System.out.println("              |");
+		System.out.println("       _______|________ ");
+		System.out.println("      /_______|________\\ \\n");
+	}
+
+} // end class
